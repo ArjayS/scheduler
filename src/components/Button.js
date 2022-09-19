6a7949d899c3694b943369439c45af781f35e7e0
Robert Servado <servadorobertgmail.com>
2022-09-19 16:07:19 -0400

    Modified Button.js from following the Compass section about running the Button in Storybook and Refactoring it

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,20 @@
 import React from "react";
-
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
-  return <></>;
+  const { confirm, danger, onClick, disabled } = props;
+
+  const buttonClass = classNames("button", {
+    "button--confirm": confirm,
+    "button--danger": danger,
+  });
+
+  return (
+    <>
+      <button className={buttonClass} onClick={onClick} disabled={disabled}>
+        {props.children}
+      </button>
+    </>
+  );
 }
