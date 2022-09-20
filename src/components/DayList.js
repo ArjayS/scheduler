0acf9e0b1c5f063d3666269e9f2fa8b0d5fe6b40
Robert Servado <servadorobertgmail.com>
2022-09-20 18:47:20 -0400

    Modified the DayList.js to refactor the way we use props as guided by Compass

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,14 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const { days, day, setDay } = props;
+  const { days, value, onChange } = props;
   return (
     <ul>
       {days.map((dayPer) => (
         <DayListItem
           key={dayPer.id}
-          selected={dayPer.name === day}
-          setDay={setDay}
+          selected={dayPer.name === value}
+          setDay={onChange}
           {...dayPer}
         />
       ))}
