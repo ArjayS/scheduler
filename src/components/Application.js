88f06de215b316a0f1973af43f21a19de6e2a738
Robert Servado <servadorobertgmail.com>
2022-09-24 23:09:02 -0400

    Modified Application.js to add connection to the api/days and api/appointments and render it in site using promise.all to access both routes and also use the helper function in selectors.js -> getAppointmentsForDay function

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import axios from "axios";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 // const days = [
 //   {
@@ -23,44 +24,44 @@ import axios from "axios";
 //   },
 // ];
 
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
+// const appointments = {
+//   1: {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   2: {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+//   3: {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   4: {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       },
+//     },
+//   },
+//   5: {
+//     id: 5,
+//     time: "4pm",
+//   },
+// };
 
 export default function Application(props) {
   // const [scheduledDay, setScheduledDay] = useState("Monday");
@@ -72,12 +73,22 @@ export default function Application(props) {
     appointments: {},
   });
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   const setDay = (day) => setState({ ...state, day });
-  const setDays = (days) => setState((prev) => ({ ...prev, days }));
+  // const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get("/api/days").then((res) => {
-      setDays(res.data);
+    const daysAPI = axios.get("/api/days");
+    const appointmentsAPI = axios.get("api/appointments");
+
+    const allPromises = [daysAPI, appointmentsAPI];
+
+    Promise.all(allPromises).then((promises) => {
+      setState((prev) => ({
+        days: promises[0].data,
+        appointments: promises[1].data,
+      }));
     });
   }, []);
 
@@ -101,7 +112,7 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appointment) => {
+        {dailyAppointments.map((appointment) => {
           return <Appointment key={appointment.id} {...appointment} />;
         })}
         <Appointment key="last" time="5pm" />
