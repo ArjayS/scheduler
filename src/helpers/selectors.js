5c2872be6b1fb0c87f0b16441570aef25a1393c6
Robert Servado <servadorobertgmail.com>
2022-09-27 16:05:09 -0400

    Modified selectors.js to include getInterviewsForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,11 +4,13 @@ const state = {
       id: 1,
       name: "Monday",
       appointments: [1, 2, 3],
+      interviewers: [1, 2],
     },
     {
       id: 2,
       name: "Tuesday",
       appointments: [4, 5],
+      interviewers: [1, 2],
     },
   ],
   appointments: {
@@ -26,7 +28,29 @@ const state = {
       interview: { student: "Chad Takahashi", interviewer: 2 },
     },
   },
+  interviewers: {
+    1: {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+    },
+    2: {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png",
+    },
+  },
 };
+
+// OUTCOME
+// {
+//   "student": "Archie Cohen",
+//   "interviewer": {
+//     id: 2,
+//     name: "Tori Malcolm",
+//     avatar: "https://i.imgur.com/Nmx0Qxo.png",
+//   }
+// }
 
 const getAppointmentsForDay = (state, day) => {
   const exactAppointmentsArr = [];
@@ -47,8 +71,48 @@ const getAppointmentsForDay = (state, day) => {
     exactAppointmentsArr.push(state.appointments[appointment]);
   }
 
-  console.log(exactAppointmentsArr);
+  // console.log(exactAppointmentsArr);
   return exactAppointmentsArr;
 };
 
-module.exports = { getAppointmentsForDay };
+const getInterview = (state, interview) => {
+  if (!interview) {
+    return null;
+  }
+
+  if (interview) {
+    const interviewTime = [interview.interviewer];
+    return {
+      student: interview.student,
+      interviewer: state.interviewers[interviewTime],
+    };
+  }
+};
+
+const getInterviewersForDay = (state, day) => {
+  const exactInterviewersArr = [];
+  const interviewersArr = [];
+
+  // Mapping through the state.days
+  state.days.map((daySelect) => {
+    // Finding the correct day inside the state.days to get the state.days.appointments array(appointmentsArr)
+    if (daySelect.name === day) {
+      console.log(daySelect.interviewers);
+      daySelect.interviewers.forEach((interviewerId) =>
+        interviewersArr.push(interviewerId)
+      );
+    }
+  });
+  console.log(interviewersArr);
+  // Getting the actual data for the state.appointments using the sorted appointmentsArr
+  for (const interviewee of interviewersArr) {
+    exactInterviewersArr.push(state.interviewers[interviewee]);
+  }
+
+  console.log(exactInterviewersArr);
+  return exactInterviewersArr;
+};
+
+// getInterviewersForDay(state, "Monday");
+
+module.exports = { getAppointmentsForDay, getInterview, getInterviewersForDay };
