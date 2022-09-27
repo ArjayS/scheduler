d9cdf989c2f801f9c967027b07dbe0d1857e97b3
Robert Servado <servadorobertgmail.com>
2022-09-27 16:08:13 -0400

    Modified the index.js for the Appointment Component to make the Form component more functional

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,20 +4,39 @@ import "./styles.scss";
 import Headers from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-  const { id, time, interview } = props;
+  const { id, time, interview, interviewers } = props;
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Headers time={time} />
-      {interview && (
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           id={id}
           student={interview.student}
-          interviewer={interview.interviewer.name}
+          interviewer={interview.interviewer}
         />
       )}
-      {!interview && <Empty />}
+      {mode === CREATE && (
+        <Form
+          student=""
+          interviewer={[]}
+          interviewers={interviewers}
+          onSave={console.log("onSave")}
+          onCancel={back}
+        />
+      )}
     </article>
   );
 }
