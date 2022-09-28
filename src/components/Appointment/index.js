32927ff575f2dbafdfdeaf15fbe6f5273a21b941
Robert Servado <servadorobertgmail.com>
2022-09-28 00:02:20 -0400

    Modified index.js for the Appointment Component to be able to pass bookInterview arguments and the function itself to be able to capture the id and interview details that we are making for a new appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,16 +5,35 @@ import Headers from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-  const { id, time, interview, interviewers } = props;
+  const { id, time, interview, interviewers, bookInterview } = props;
 
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
+  const save = (name, interviewer) => {
+    if ((name, interviewer)) {
+      transition(SAVING);
+    }
+
+    const interview = {
+      student: name,
+      interviewer,
+    };
+
+    const bookPromise = bookInterview(id, interview);
+    console.log(bookPromise);
+    bookPromise.then(() => {
+      transition(SHOW);
+    });
+  };
 
   return (
     <article className="appointment">
@@ -31,12 +50,13 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form
           student=""
-          interviewer={[]}
+          // interviewer={[]}
           interviewers={interviewers}
-          onSave={console.log("onSave")}
+          onSave={save}
           onCancel={back}
         />
       )}
+      {mode === SAVING && <Status message="Saving" />}
     </article>
   );
 }
