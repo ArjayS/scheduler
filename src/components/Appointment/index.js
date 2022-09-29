af62b5ae82a3ebdfa541be0fd47c6768842416ca
Robert Servado <servadorobertgmail.com>
2022-09-29 00:09:44 -0400

    Modified the index.js for the Appointment Component to include features and functionality of Deleting an Appointment, Editing an Appointment, handling Error states when an error occurs in saving and deleting an appointment. Also, added appropriate modes to render in the front-end

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,57 +6,114 @@ import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
 import Status from "./Status";
+import Confirm from "./Confirm";
+import Error from "./Error";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-  const { id, time, interview, interviewers, bookInterview } = props;
+  const { id, time, interview, interviewers, bookInterview, cancelInterview } =
+    props;
 
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
+  const CONFIRM = "CONFIRM";
+  const DELETING = "DELETING";
+  const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
   const save = (name, interviewer) => {
-    if ((name, interviewer)) {
-      transition(SAVING);
-    }
-
     const interview = {
       student: name,
       interviewer,
     };
 
+    if ((name, interviewer)) {
+      transition(SAVING);
+    }
+
     const bookPromise = bookInterview(id, interview);
     console.log(bookPromise);
-    bookPromise.then(() => {
-      transition(SHOW);
-    });
+    bookPromise
+      .then(() => {
+        transition(SHOW);
+      })
+      .catch((error) => {
+        transition(ERROR_SAVE, true);
+      });
   };
+
+  const remove = () => {
+    if (mode === CONFIRM) {
+      transition(DELETING, true);
+      const removePromise = cancelInterview(id);
+      removePromise
+        .then(() => {
+          transition(EMPTY);
+        })
+        .catch((error) => {
+          transition(ERROR_DELETE, true);
+        });
+    } else {
+      transition(CONFIRM);
+    }
+  };
+
+  function edit() {
+    transition(EDIT);
+  }
 
   return (
     <article className="appointment">
       <Headers time={time} />
 
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && (
+      {mode === SHOW && interview && (
         <Show
           id={id}
           student={interview.student}
           interviewer={interview.interviewer}
+          onDelete={remove}
+          onEdit={edit}
         />
       )}
       {mode === CREATE && (
+        <Form interviewers={interviewers} onSave={save} onCancel={back} />
+      )}
+      {mode === SAVING && <Status message="Saving" />}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you would like to delete?"
+          onCancel={back}
+          onConfirm={remove}
+        />
+      )}
+      {mode === DELETING && <Status message="Deleting" />}
+      {mode === EDIT && (
         <Form
-          student=""
-          // interviewer={[]}
+          student={interview.student}
+          interviewer={interview.interviewer}
           interviewers={interviewers}
           onSave={save}
           onCancel={back}
         />
       )}
-      {mode === SAVING && <Status message="Saving" />}
+      {mode === ERROR_SAVE && (
+        <Error
+          message="Error: Could not save your appointment"
+          onClose={back}
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error
+          message="Error: Could not delete your appointment"
+          onClose={back}
+        />
+      )}
     </article>
   );
 }
