30c4659362dbb63c6ce2f3fe62b4950fa07d0b67
Robert Servado <servadorobertgmail.com>
2022-09-29 00:08:40 -0400

    Modified the Application.js to include features and functionality of Deleting an Appointment, Editing an Appointment, handling Error states when an error occurs in saving and deleting an appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,63 +10,6 @@ import {
   getInterviewersForDay,
 } from "helpers/selectors";
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
-// const appointments = {
-//   1: {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   2: {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   3: {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   4: {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       },
-//     },
-//   },
-//   5: {
-//     id: 5,
-//     time: "4pm",
-//   },
-// };
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
@@ -75,18 +18,35 @@ export default function Application(props) {
     interviewers: {},
   });
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewers = getInterviewersForDay(state, state.day);
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const setDay = (day) => setState({ ...state, day });
 
   const bookInterview = (id, interview) => {
     return axios.put(`api/appointments/${id}`, { interview }).then((res) => {
-      console.log(id, interview);
-
       const appointment = {
         ...state.appointments[id],
         interview: { ...interview },
+      };
+
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment,
+      };
+
+      setState({ ...state, appointments });
+    });
+  };
+
+  const cancelInterview = (id) => {
+    return axios.delete(`api/appointments/${id}`).then((res) => {
+      console.log(id);
+
+      const appointment = {
+        ...state.appointments[id],
+        interview: null,
       };
 
       const appointments = {
@@ -149,6 +109,7 @@ export default function Application(props) {
               interviewers={dailyInterviewers}
               interview={interview}
               bookInterview={bookInterview}
+              cancelInterview={cancelInterview}
             />
           );
         })}
