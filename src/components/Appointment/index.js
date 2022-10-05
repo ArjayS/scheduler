608c21b5699f178284b9eca7f3a6651befc6645d
Robert Servado <servadorobertgmail.com>
2022-10-05 18:00:07 -0400

    Modified the index.js and add the data-testid='appointment' in the article tag

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -68,7 +68,7 @@ export default function Appointment(props) {
   }
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <Headers time={time} />
 
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
